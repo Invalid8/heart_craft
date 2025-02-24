0633b1acce6c194b763a7bb37e33bddb512bcb1f
dalgoridim <b.fadamitan2019@gmail.com>
2025-02-25 00:17:17 +0100

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap');
+</style>
